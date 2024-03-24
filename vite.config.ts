c93a49a0fdf43f0d7cd18add749abd9c0add7d8a
Scott Voyles <zeitchef@gmail.com>
2024-03-24 20:02:49 +0100

    feat: vercel should read vite env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: ['./tests/setup.ts'],
-    coverage: {},
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'happy-dom',
+      setupFiles: ['./tests/setup.ts'],
+      coverage: {},
+    },
+  }
 })
